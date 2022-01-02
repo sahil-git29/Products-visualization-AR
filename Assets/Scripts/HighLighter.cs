aae6e086189035f6130d93e9be9d66dc94e1f3b3
Sahil Gulati <39591887+sahil-git29@users.noreply.github.com>
2022-01-02 17:14:07 +0530

    AR visualization of Prducts

---
+++ Assets/Scripts/HighLighter.cs
@@ -0,0 +1,17 @@
+﻿
+s;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighLighter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
